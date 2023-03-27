b20738a4319fc85a55885056a0dcb634324d67f1
Limeng-svg <73696606+Limeng-svg@users.noreply.github.com>
2023-03-27 11:06:54 +0800

    Add files via upload

---
+++ rtfwz/src/main/java/com/zyl/cfwz/CfwzApplication.java
@@ -0,0 +1,13 @@
+package com.zyl.cfwz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfwzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfwzApplication.class, args);
+    }
+
+}
---
+++ rtfwz/src/test/java/com/zyl/cfwz/CfwzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyl.cfwz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfwzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
